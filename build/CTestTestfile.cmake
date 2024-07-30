ad70b00cfd87aed03090e32e3d531ff22c14ed15
maoMaocongcong <mhpan.tju@gmail.com>
2024-07-30 14:00:08 -0700

    add cmake ci

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/maocong/Documents/c++/Distributed-System
+# Build directory: /Users/maocong/Documents/c++/Distributed-System/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(runtest "/Users/maocong/Documents/c++/Distributed-System/build/runtest")
+set_tests_properties(runtest PROPERTIES  _BACKTRACE_TRIPLES "/Users/maocong/Documents/c++/Distributed-System/CMakeLists.txt;65;add_test;/Users/maocong/Documents/c++/Distributed-System/CMakeLists.txt;0;")
